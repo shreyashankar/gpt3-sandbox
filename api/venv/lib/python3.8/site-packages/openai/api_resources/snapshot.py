7364a5ffe74da801659b5494aba05137cb1a0064
Shreya <ss.shankar505@gmail.com>
2020-07-19 11:50:59 -0500

    Removing venv

--- api/venv/lib/python3.8/site-packages/openai/api_resources/snapshot.py
+++
@@ -1,7 +0,0 @@
-from openai.api_resources.abstract.engine_api_resource import EngineAPIResource
-from openai.api_resources.abstract import ListableAPIResource, DeletableAPIResource
-
-
-class Snapshot(EngineAPIResource, ListableAPIResource, DeletableAPIResource):
-    engine_required = False
-    OBJECT_NAME = "snapshot"
