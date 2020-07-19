7961e9af47c0c51db4d2139542deca47be8542bb
Shreya <ss.shankar505@gmail.com>
2020-07-19 17:19:39 -0500

    Moved to examples dir.

---
+++ examples/run_analogies_app.py
@@ -0,0 +1,25 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from api.demo_web_app import demo_web_app
+from api.GPT import GPT, Example
+from api.UIConfig import UIConfig
+
+# Construct GPT object and show some examples
+gpt = GPT(engine="davinci",
+          temperature=0.5,
+          max_tokens=100)
+
+gpt.add_example(Example('Neural networks are like', 'genetic algorithms in that both are systems that learn from experience.'))
+gpt.add_example(Example('Social media is like', 'a market in that both are systems that coordinate the actions of many individuals.'))
+gpt.add_example(Example('A2E is like', 'lipofuscin in that both are byproducts of the normal operation of a system.'))
+gpt.add_example(Example('Haskell is like', 'LISP in that both are functional languages.'))
+gpt.add_example(Example('Quaternions are like', 'matrices in that both are used to represent rotations in three dimensions.'))
+gpt.add_example(Example('Quaternions are like', 'octonions in that both are examples of non-commutative algebra.'))
+
+# Define UI configuration
+config = UIConfig(description="Analogies generator",
+                  buttonText="Generate",
+                  placeholder="Memes are like")
+
+demo_web_app(gpt, config)
