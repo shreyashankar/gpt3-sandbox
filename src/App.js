e93a0e2d8fc962479fe84eeece56d3ead8015a1a
buyumaz <bora@viaduct.ai>
2020-07-19 11:48:48 -0400

    removed prime text from front end

--- src/App.js
+++ src/App.js
@@ -15,9 +15,6 @@ class App extends React.Component {
       latexStr: '',
       value: 'x squared plus two times x' };
 
-    this.primeText = `English: Two plus two equals four\nLaTeX: 2 + 2 = 4\n\nEnglish: The integral from zero to infinity\nLaTeX: \\int_0^{\\infty}\n\nEnglish: The gradient of x squared plus two times x with respect to x\nLaTeX: \\nabla_x x^2 + 2x\n\nEnglish: The log of two times x\nLaTeX: \\log{2x}\n\nEnglish: x squared plus y squared plus equals z squared\nLaTeX: x^2 + y^2 = z^2\n\nEnglish: The sum from zero to twelve of i squared\nLaTeX: \\sum_{i=0}^{12} i^2\n\nEnglish: E equals m times c squared\nLaTeX: E = mc^2\n\nEnglish: H naught of t\nLaTeX: H_0(t)\n\nEnglish: f of n equals 1 over (b-a) if n is 0 otherwise 5\nLaTeX: f(n) = \\begin{cases} 1/(b-a) &\\mbox{if } n \\equiv 0 \\\
-    5 \\end{cases}\n\n`;
-
     // This binding is necessary to make `this` work in the callback
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
