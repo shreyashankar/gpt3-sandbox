1663b25a3ad366ccd5ca0d4e1a2adc2a320a5e25
buyumaz <bora@viaduct.ai>
2020-07-19 14:01:16 -0400

    refactored back end to isolate app code and logic

---
+++ api/demo_web_app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import openai
+
+def submit_openai_request(prime_text, prompt):
+    query = prime_text + "input: " + prompt + "\n"
+    response = openai.Completion.create(engine="davinci",
+                                        prompt=query,
+                                        max_tokens=100,
+                                        temperature=0.5,
+                                        top_p=1,
+                                        n=1,
+                                        stream=False,
+                                        stop="\ninput:") 
+    return response
+    
+
+def demo_web_app(gpt):
+    prime_text = gpt.get_prime_text()
+    app = Flask(__name__)
+    app.config.from_envvar('LATEX_TRANSLATOR_CONFIG')
+    CORS(app)
+    openai.api_key = app.config['OPENAI_KEY']
+
+
+    @app.route("/translate", methods=['GET', 'POST'])
+    def translate():
+        prompt = request.json['prompt']
+        response = submit_openai_request(prime_text, prompt)
+        return {'text': response['choices'][0]['text'][7:]}
+
+    app.run()
