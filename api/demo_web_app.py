b543f2767218f54c3db5b9c411c4984d7ce8f554
buyumaz <bora@viaduct.ai>
2020-07-19 15:27:09 -0400

    added config options to GPT class

--- api/demo_web_app.py
+++ api/demo_web_app.py
@@ -4,12 +4,11 @@ import openai
 
 from .UIConfig import UIConfig
 
-def submit_openai_request(prime_text, prompt):
-    query = prime_text + "input: " + prompt + "\n"
-    response = openai.Completion.create(engine="davinci",
-                                        prompt=query,
-                                        max_tokens=100,
-                                        temperature=0.5,
+def submit_openai_request(gpt, prompt):
+    response = openai.Completion.create(engine=gpt.get_engine(),
+                                        prompt=gpt.craft_query(prompt),
+                                        max_tokens=gpt.get_max_tokens(),
+                                        temperature=gpt.get_temperature(),
                                         top_p=1,
                                         n=1,
                                         stream=False,
@@ -18,7 +17,6 @@ def submit_openai_request(prime_text, prompt):
     
 
 def demo_web_app(gpt, config=UIConfig()):
-    prime_text = gpt.get_prime_text()
     app = Flask(__name__)
     app.config.from_envvar('LATEX_TRANSLATOR_CONFIG')
     CORS(app)
@@ -32,7 +30,7 @@ def demo_web_app(gpt, config=UIConfig()):
     @app.route("/translate", methods=['GET', 'POST'])
     def translate():
         prompt = request.json['prompt']
-        response = submit_openai_request(prime_text, prompt)
+        response = submit_openai_request(gpt, prompt)
         return {'text': response['choices'][0]['text'][7:]}
 
     app.run()
